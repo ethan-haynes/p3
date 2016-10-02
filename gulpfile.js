e06b4bd84d75c0e177148408fbe7a4e8de5737fd
ethan <ehaynes@g.harvard.edu>
2016-10-02 13:41:02 -0500

    setting up project

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,13 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+    mix.styles('style.css');
+    mix.styles('bootstrap.min.css',
+        'public/css/',
+        'vendor/twbs/bootstrap/dist/css'
+    );
+    mix.scripts('bootstrap.min.js',
+        'public/js/',
+        'vendor/twbs/bootstrap/dist/js'
+    );
 });
---
+++ resources/assets/js/main.js
@@ -0,0 +1,8 @@
+(function($) {
+    'use strict';
+
+    //Document ready
+    $(function() {
+        
+    });
+})(jQuery);
