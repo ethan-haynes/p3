c7f75de7815090ea3114f39e3cab5a4797aed4b6
ethan <ehaynes@g.harvard.edu>
2016-10-02 13:51:34 -0500

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,6 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.styles('style.css');
-    mix.styles('bootstrap.min.css',
-        'public/css/',
-        'vendor/twbs/bootstrap/dist/css'
-    );
-    mix.scripts('bootstrap.min.js',
-        'public/js/',
-        'vendor/twbs/bootstrap/dist/js'
-    );
+    mix.scripts('bootstrap.min.js');
+    mix.scripts('main.js');
 });
---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+(function($) {
+    'use strict';
+
+    //Document ready
+    $(function() {
+        
+    });
+})(jQuery);
+
+//# sourceMappingURL=main.js.map
